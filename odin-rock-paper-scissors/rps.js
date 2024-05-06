63292e52547e3782781ee37b29e6805b6dedea5d
CodeLotusGX7 <114036124+CodeLotusGX7@users.noreply.github.com>
2024-05-06 14:22:45 -0400

    computer randomly puts out rock paper on scissors to console

--- odin-rock-paper-scissors/rps.js
+++ odin-rock-paper-scissors/rps.js
@@ -1,1 +1,24 @@
 console.log("Hello Friend");
+
+function getComputerChoice(){
+    let r = "Rock"
+    let p = "Papper"
+    let s = "Scissors"
+    let a = 1;
+
+    //Math.random()
+    let x = Math.floor((Math.random() * 3) + 1);
+
+    if (x == 1){
+        console.log(r);
+    } else if (x == 2){
+        console.log(p);
+    } else if (x == 3){
+        console.log(s);
+    } else{
+        // return x;
+    }
+    
+
+}
+console.log(getComputerChoice());
