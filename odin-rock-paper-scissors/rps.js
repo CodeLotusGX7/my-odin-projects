94f95bdfbf3fc7e907fb59da2edd25a22bc2d3b1
CodeLotusGX7 <114036124+CodeLotusGX7@users.noreply.github.com>
2024-05-06 15:16:56 -0400

    made human input case-insensitive

--- odin-rock-paper-scissors/rps.js
+++ odin-rock-paper-scissors/rps.js
@@ -1,5 +1,9 @@
 console.log("Hello Friend");
 
+// global scope variables
+let humanScore = 0;
+let computerScore = 0;
+
 // spits out r,p, or s
 function getComputerChoice(){
     let r = "Rock"
@@ -22,11 +26,22 @@ function getComputerChoice(){
     
 
 }
-
+// taking human input
 function getHumanChoice(){
-    let x = prompt("Please enter Rock, Paper, or Scissors", "Rock");
+    let x = prompt("Please enter Rock, Paper, or Scissors", "Rock").toLowerCase();
 
     return x;
 }
-console.log(getComputerChoice());
+
+// logic for a round
+function playRound(humanChoice, computerChoice){
+  
+
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+//console.log(getComputerChoice());
 console.log(getHumanChoice());
+// console.log(humanSelection);
+//playRound(humanSelection, computerSelection);
