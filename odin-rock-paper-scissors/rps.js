9b888bbdff594093b34eb947a16c6d0729d1b7fe
CodeLotusGX7 <114036124+CodeLotusGX7@users.noreply.github.com>
2024-05-06 21:34:34 -0400

    created playGame function and moved playRound logic into it

--- odin-rock-paper-scissors/rps.js
+++ odin-rock-paper-scissors/rps.js
@@ -6,9 +6,9 @@ let computerScore = 0;
 
 // spits out r,p, or s
 function getComputerChoice(){
-    let r = "Rock"
-    let p = "Papper"
-    let s = "Scissors"
+    let r = "rock"
+    let p = "papper"
+    let s = "scissors"
     let a = 1;
 
     //Math.random()
@@ -28,20 +28,41 @@ function getComputerChoice(){
 }
 // taking human input
 function getHumanChoice(){
-    let x = prompt("Please enter Rock, Paper, or Scissors", "Rock").toLowerCase();
+    let x = prompt("Please enter Rock, Paper, or Scissors", ).toLowerCase();
 
     return x;
 }
 
-// logic for a round
+function playGame(){
+    // logic for a round
 function playRound(humanChoice, computerChoice){
+    let currentRound = 1;
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
   
+  
+      
+    let roundWinner;
+    if (humanSelection == computerSelection){
+      console.log("it's a tie");
+    } else{
+      console.log("it's not working")
+    }
+      currentRound++;
+      return console.log("current round: " + currentRound);
+  
+  }
+  //const humanSelection = getHumanChoice();
+  //const computerSelection = getComputerChoice();
+  
+  //console.log(getComputerChoice());
+  // console.log(getHumanChoice());
+  //console.log(round);
+  // console.log(humanSelection);
+  //playRound(humanSelection, computerSelection);
+
+  // calls playRound 5x
+
 
 }
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
-//console.log(getComputerChoice());
-console.log(getHumanChoice());
-// console.log(humanSelection);
-//playRound(humanSelection, computerSelection);
