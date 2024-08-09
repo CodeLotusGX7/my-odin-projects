d32f298e9786b7d922f4b8cadd3f15a459da23ed
CodeLotusGX7 <114036124+CodeLotusGX7@users.noreply.github.com>
2024-08-09 08:10:52 -0400

    views section complete

--- odin-practice-files/second-express-app/app.js
+++ odin-practice-files/second-express-app/app.js
@@ -1,8 +1,26 @@
+// app.js
 const express = require('express');
+//const ejs = require('ejs');
 const path = require("node:path");
-
 const app = express();
 const port = 8080;
+const links = [
+    { href: "/", text: "Home" },
+    { href: "about", text: "About" },
+];
+const users = ["Rose", "Cake", "Biff"];
+const assetsPath = path.join(__dirname, "public");
+
+app.use(express.static(assetsPath));
+
+// routing
+app.get("/", (req, res) => {
+    res.render("index", { links: links });
+});
+
+app.get("/", (req, res) => {
+    res.render("index", { links: links, users: users });
+});
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
