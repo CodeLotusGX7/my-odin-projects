67bd7297b322f819a2b5b0ef85445506c5214059
CodeLotusGX7 <114036124+CodeLotusGX7@users.noreply.github.com>
2024-08-10 09:03:45 -0400

    setup views engine

--- odin-mini-messageboard/app.js
+++ odin-mini-messageboard/app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require("node:path"); // view engine
 
-app.get("/", (req, res) => res.send("Hello, friend"));
+//app.get("/", (req, res) => res.send("Hello, friend"));
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
+// ejs
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index", {message: "EJS rocks!"});
+});
